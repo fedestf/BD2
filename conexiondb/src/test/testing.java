346b36b59180d754ca91b9f6d5c8e1bd46d70f4f
fedestf <fede520@live.com>
2020-04-20 16:16:45 -0300

    -
    -

--- conexiondb/src/test/testing.java
+++ conexiondb/src/test/testing.java
@@ -3,6 +3,7 @@ import codigo.*;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 
 
@@ -13,8 +14,11 @@ public class testing {
 
     public static void main(String[] args) {     
     	
-    	MongoClient mongoClient = new MongoClient();
-    	MongoDatabase database = mongoClient.getDatabase("mydb");
-  
+    	MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
+    	MongoClient mongoClient = new MongoClient(connectionString);
+    	
+    	MongoDatabase database = mongoClient.getDatabase("local");
+    	
+    	System.out.println(database.getName());
     }    
 }
